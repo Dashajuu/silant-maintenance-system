5c09e07ad8ab7964e45539092bc1fc9843128acb
DashaJuu <dashaju_u@mail.ru>
2025-07-23 12:50:27 +0300

    add service data

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', MachineListView.as_view(), name='home_page'),
     path('catalogs', views.get_manager_context_data, name='mangers_catalog'),
     path('list_accounts', views.get_manager_accounts_context_data, name='create_account'),
+    path('service', views.get_maintenance_complaints_data, name='service_catalog'),
 ]
--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -1,9 +1,11 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from accounts import models as account
 from machines import models as machine
 from technical_service import models as maintenance
 from complaints import models as complaint
+
 
 def get_manager_context_data(request):
     data = {
@@ -30,3 +32,44 @@ def get_manager_accounts_context_data(request):
     }
 
     return render(request, 'main/manager_account_data.html', context=data)
+
+
+def get_maintenance_complaints_data(request):
+    user = request.user
+    group = user.groups.first()
+
+    if not group:
+        raise Http404("Группа пользователя не определена")
+
+    role = group.name
+    context = {}
+
+    if role == "managers":
+        try:
+            manager = account.Manager.objects.get(user=user)
+            clients = account.Client.objects.filter(manager=manager)
+            machines = machine.Machine.objects.filter(client__in=clients)
+        except account.Manager.DoesNotExist:
+            raise Http404("Менеджер не найден")
+
+    elif role == "clients":
+        try:
+            client = account.Client.objects.get(user=user)
+            machines = machine.Machine.objects.filter(client=client)
+        except account.Client.DoesNotExist:
+            raise Http404("Клиент не найден")
+
+    elif role == "service_companies":
+        try:
+            service_company = account.ServiceCompany.objects.get(user=user)
+            machines = machine.Machine.objects.filter(service_company=service_company)
+        except account.ServiceCompany.DoesNotExist:
+            raise Http404("Сервисная компания не найдена")
+
+    else:
+        raise Http404("Неизвестная роль пользователя")
+
+    context['maintenances'] = maintenance.Maintenance.objects.filter(machine__in=machines)
+    context['complaints'] = complaint.Complaint.objects.filter(machine__in=machines)
+
+    return render(request, 'main/service.html', context)
